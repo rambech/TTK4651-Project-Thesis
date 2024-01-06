2f8800f0a63bdabd829bf12a997f9797c336f1b0
rambech <thatalexram@gmail.com>
2024-01-06 08:10:25 +0100

    Higher success reward

--- AlexandersSimplifiedVehicleSimulator/rl/env/forwarddocking.py
+++ AlexandersSimplifiedVehicleSimulator/rl/env/forwarddocking.py
@@ -230,7 +230,9 @@ class ForwardDockingEnv(Env):
         if self.success():
             terminated = True
             lower_reward_limit = 10000
-            success_time_reward = 10*(self.step_limit - self.step_count)
+            # Time not spent must be rewarded more than time
+            # spent into the quay
+            success_time_reward = 20*(self.step_limit - self.step_count)
             reward = max(lower_reward_limit, success_time_reward)
 
         if self.time_out():
