f2c0af4a51bcd60e8d160731f9abf302e30e8d6a
rambech <thatalexram@gmail.com>
2024-01-08 10:42:05 +0100

    Add more plotting functionality

--- AlexandersSimplifiedVehicleSimulator/continue.py
+++ AlexandersSimplifiedVehicleSimulator/continue.py
@@ -1,10 +1,10 @@
 import os
+import time
 import json
 import numpy as np
 from rl.env import ForwardDockingEnv, DPEnv, SidewaysDockingEnv
 from vehicle import Otter
-from maps import SimpleMap, Target
-from utils import D2R
+from maps import SimpleMap
 from stable_baselines3.common.callbacks import CheckpointCallback
 
 # TODO: Make every model save to a new numbered folder,
@@ -12,7 +12,7 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 # TODO: Add a settings file and put it into model folder
 
 # Training settings
-model_type = "TD3"
+model_type = "PPO"
 env_type = "docking"
 random_weather = False
 seed = 1
@@ -21,27 +21,19 @@ timestep_multiplier = 5
 SECONDS = 120
 VEHICLE_FPS = 60
 RL_FPS = 20
-EPISODES = 5000
+EPISODES = 7500
 TIMESTEPS = SECONDS*RL_FPS*EPISODES  # *timestep_multiplier
-print(f"Timesteps: {TIMESTEPS}")
 
-test_name = input(f"Test name is {model_type}-{env_type}-")
-test_name = f"{model_type}-{env_type}-{test_name}"
+# test_name = input(f"Test name is {model_type}-{env_type}-")
+# test_name = f"{model_type}-{env_type}-{test_name}"
 
 models_dir = "models"
+test_name = "PPO-docking-71-c"
+load_iteration = "11856000"
 log_dir = "logs"
 model_path = f"{models_dir}/{test_name}"
 log_path = f"{log_dir}/{test_name}"
-
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
-
-if not os.path.exists(log_path):
-    os.makedirs(log_path)
-
-file_name = f"{test_name}.json"
-file_path = os.path.join(model_path, file_name)
-
+model_path = f"{models_dir}/{test_name}/{test_name}_{load_iteration}_steps.zip"
 
 # Initialize vehicle
 vehicle = Otter(dt=1/VEHICLE_FPS)
@@ -65,25 +57,6 @@ elif env_type == "DP":
     env = DPEnv(vehicle, map, seed, render_mode=None,
                 FPS=RL_FPS, threshold=threshold, random_weather=random_weather)
 
-data = {
-    "Test_name": test_name,
-    "Model type": model_type,
-    "Env type": env_type,
-    "Random Weather": random_weather,
-    "Vehicle fps": VEHICLE_FPS,
-    "RL fps": RL_FPS,
-    "Episodes": EPISODES,
-    "Timesteps": TIMESTEPS,
-    "Threshold": threshold,
-    "Seed": seed,
-    "Initial pose": eta_init.tolist(),
-    "Commit hash": "main 7b0ab9f"
-}
-
-# Save the dictionary to the file
-with open(file_path, 'w') as json_file:
-    json.dump(data, json_file, indent=2)
-
 checkpoint_callback = CheckpointCallback(
     save_freq=RL_FPS*SECONDS*10,    # Save every tenth episode
     save_path=model_path,
@@ -96,12 +69,12 @@ env.reset(seed)
 if model_type == "PPO":
     from stable_baselines3 import PPO
 
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_path)
+    model = PPO.load(model_path, env=env)
 
 if model_type == "TD3":
     from stable_baselines3 import TD3
 
-    model = TD3("MlpPolicy", env, verbose=1, tensorboard_log=log_path)
+    model = TD3.load(model_path, env=env)
 
 
 model.learn(total_timesteps=TIMESTEPS,
